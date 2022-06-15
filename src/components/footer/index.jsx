811e88846af47a9eab19c32a1c3e924b21880fb5
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-06-15 23:48:44 +0600

    gold level completed, new search icon

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+export const Footer = ({ fan }) => {
+    return (
+      <>
+        <div className="footer-wrapper">
+            <span className='Text footer-element'>Nurbek</span>
+            <a href="https://google.com" className='link1 footer-element'>Lenovo</a>
+            <a href="https://twitter.com" className='link2 footer-element'>MacBook</a>
+        </div>
+      </>
+    );
+  };
+  
